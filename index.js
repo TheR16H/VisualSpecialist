2927ed9cee914558b4e4712e98a3c7021583fd87
TheR16H <rashawn5hall@gmail.com>
2024-06-19 10:49:43 -0400

    test logic / importing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // import the shape classes from shapes.js in the lib folder. Use those classes to run the application logic or any other functionality
 
-import 
+const { Square, Circle, Triangle} = require('./lib/shapes');
+ 
